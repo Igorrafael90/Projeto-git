16938401dda29c5a51ba5258b2932ba17ba0fe26
Igor <rafaeligor804@gmail.com>
2023-04-20 11:17:25 -0300

    inicio

---
+++ modulos.py
@@ -0,0 +1,22 @@
+def leropc(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print("O usuario digitou um escolha fora dos parametros, volte e escolha um deles")
+            continue
+        except (KeyboardInterrupt):
+            print("O usuario não digitou nenhuma esolha entre os parametros, volte e escolha um deles")
+            return 0
+        else:
+            return n
+        
+def menu(lista):
+    print("=====MENU======")
+    c = 0
+    for item in lista:
+        print(f"{c} > {item}")
+        c += 1
+    print("===============")
+    opc = leropc("Digite sua escolha: ")
+    return opc
